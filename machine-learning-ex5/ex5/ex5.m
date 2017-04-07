7eeb3eb4bb8ef53b5aad8335a71dcc376b68c9e1
paulvercoustre <paulvercoustre@gmail.com>
2017-04-08 00:17:39 +0200

    Part 1 to 3

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -164,7 +164,7 @@ pause;
 %  lambda to see how the fit and learning curve change.
 %
 
-lambda = 0;
+lambda = 100;
 [theta] = trainLinearReg(X_poly, y, lambda);
 
 % Plot training data and fit
@@ -174,6 +174,9 @@ plotFit(min(X), max(X), mu, sigma, theta, p);
 xlabel('Change in water level (x)');
 ylabel('Water flowing out of the dam (y)');
 title (sprintf('Polynomial Regression Fit (lambda = %f)', lambda));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
 figure(2);
 [error_train, error_val] = ...
--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,16 +19,14 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% compute regularisation term
+regulariser = vertcat(0, theta(2:end, :))' * theta;
 
+% compute cost function
+J = 1/(2*m) * sum((X * theta - y).^2) + lambda/(2*m) * regulariser ;
 
-
-
-
-
-
-
-
-
+% compute gradient
+grad = (1/m) * X' * (X * theta - y) + (lambda/m) * vertcat(0, theta(2:end, :));
 
 % =========================================================================
 
